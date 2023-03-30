15e84f625adf100716e15b97aedaa656c215a851
George Samuel <geosam2708@gmail.com>
2023-03-30 23:59:05 +0300

    updated variables

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 // Your code here
-//add an event listener of DOMContent loaded
-document.addEventListener('DOMContentLoaded', () => {
-    
-    // fetch name and display
-    fetchAndDisplay("http://localhost:3000/characters")
 
-    function fetchAndDisplay(URL){
-        fetch(URL)
-        .then((response) => response.json())
-        .then((results) => {
-            for(const result of results)appendSpan(result.name,result.image)
-        })
-    }
-
-    //add newcharacters
-    let newCharacter = document.querySelector('.characterInfo').lastElementChild
-    newCharacter.addEventListener('submit', addCharacter)
-})
-
-
+//grab elements
+let characterBar = document.querySelector('#character-bar')
+let detailedInfo = characterBar.nextElementSibling.firstElementChild
+let votes = detailedInfo.querySelector('h4').firstElementChild
 
 //sumbmit votes
 let totalVotes = 0
@@ -27,21 +12,19 @@ function submitVotes(event){
     let input = event.target.votes
     let value = parseInt(input.value, 10)
     totalVotes += value
-    let voteSelect = event.target.previousElementSibling.firstElementChild
-    voteSelect.innerText = totalVotes
+    votes.innerText = totalVotes
     input.value =''
 
     //clear votes when button is clicked
-    let reset = document.querySelector('.characterInfo').firstElementChild.lastElementChild
+    let reset = detailedInfo.lastElementChild
     reset.addEventListener('click', resetVotes)
 }
 
 //add span and append
 function appendSpan(name, image){
     let span = document.createElement('span')
-    let parent = document.querySelector('#character-bar')
     span.innerText =name
-    parent.appendChild(span)
+    characterBar.appendChild(span)
 
     //click listener updating DOM
     span.addEventListener('click', () =>{
@@ -67,18 +50,37 @@ function addCharacter(event){
 
 //display characters
 function display(name, image){
-    let characterName = document.querySelector('.characterInfo').firstElementChild.firstElementChild
+    let characterName = detailedInfo.firstElementChild
     let characterImage = characterName.nextElementSibling
     characterName.textContent = name
     characterImage.src = image
     characterImage.nextElementSibling.firstElementChild.innerText = 0
 
     // grab form to update votes based on character
-    let form = document.querySelector('.characterInfo').firstElementChild.lastElementChild.previousElementSibling
+    let form = detailedInfo.querySelector('form')
     form.addEventListener('submit', submitVotes)
 }
 
 //reset votes
 function resetVotes(){
-    document.querySelector('.characterInfo').firstElementChild.querySelector('h4').firstElementChild.innerText = 0
+    detailedInfo.querySelector('h4').firstElementChild.innerText = 0
 }
+
+//add an event listener of DOMContent loaded
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // fetch name and display
+    fetchAndDisplay("http://localhost:3000/characters")
+
+    function fetchAndDisplay(URL){
+        fetch(URL)
+        .then((response) => response.json())
+        .then((results) => {
+            for(const result of results)appendSpan(result.name,result.image)
+        })
+    }
+
+    //add newcharacters
+    let newCharacter = document.querySelector('.characterInfo').lastElementChild
+    newCharacter.addEventListener('submit', addCharacter)
+})
