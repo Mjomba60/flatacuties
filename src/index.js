1379142d1ea15da6a3a5aac7813a165f408af0f4
George Samuel <geosam2708@gmail.com>
2023-03-30 23:31:20 +0300

    updated functions

--- src/index.js
+++ src/index.js
@@ -2,46 +2,83 @@
 //add an event listener of DOMContent loaded
 document.addEventListener('DOMContentLoaded', () => {
     
-    //Grab elements to manipulate and display
-    let characterbar = document.getElementById('character-bar')
-    let totalvote = 0;
-    
-    let character = document.querySelector('.characterInfo')
-    let details = character.firstElementChild
-    let name = details.firstElementChild
-    let image = details.getElementsByTagName('img').item(0)
-    let total = details.getElementsByTagName('h4').item(0).firstElementChild
-    let form = details.getElementsByTagName('form').item(0)
     // fetch name and display
-    fetchAndDisplay("http://localhost:3000/characters", characterbar)
+    fetchAndDisplay("http://localhost:3000/characters")
 
-    function fetchAndDisplay(URL, element){
+    function fetchAndDisplay(URL){
         fetch(URL)
         .then((response) => response.json())
         .then((results) => {
-            for(const result of results){
-                let span = document.createElement('span')
-                span.innerText = result.name
-                element.appendChild(span)
-
-                //add event listener to the sapn
-                span.addEventListener('click', () => {
-                    name.textContent = result.name
-                    image.src = result.image
-                    total.innerText = 0
-
-                    //add an event listener to the form
-                    form.addEventListener('submit', (e) => {
-                        e.preventDefault()
-                        
-                        let value = parseInt(form.firstElementChild.value, 10)
-                        totalvote += value
-                        total.innerText = totalvote    
-                    })
-
-                })
-            }
+            for(const result of results)appendSpan(result.name,result.image)
         })
     }
-    
+
+    //add newcharacters
+    let newCharacter = document.querySelector('.characterInfo').lastElementChild
+    newCharacter.addEventListener('submit', addCharacter)
 })
+
+
+
+//sumbmit votes
+let totalVotes = 0
+function submitVotes(event){
+    event.preventDefault()
+    let input = event.target.votes
+    let value = parseInt(input.value, 10)
+    totalVotes += value
+    let voteSelect = event.target.previousElementSibling.firstElementChild
+    voteSelect.innerText = totalVotes
+    input.value =''
+
+    //clear votes when button is clicked
+    let reset = document.querySelector('.characterInfo').firstElementChild.lastElementChild
+    reset.addEventListener('click', resetVotes)
+}
+
+//add span and append
+function appendSpan(name, image){
+    let span = document.createElement('span')
+    let parent = document.querySelector('#character-bar')
+    span.innerText =name
+    parent.appendChild(span)
+
+    //click listener updating DOM
+    span.addEventListener('click', () =>{
+        display(name,image)
+    })
+}
+
+//add a new character
+function addCharacter(event){
+    event.preventDefault()
+    let name = event.target.firstElementChild.lastElementChild
+    let url = event.target.firstElementChild.nextElementSibling.lastElementChild
+    let character = {}
+    character.name = name.value
+    character.image = url.value
+    appendSpan(name.value, url.value)
+    display(name.value, url.value)
+
+    // clear inputs
+    name.value = ''
+    url.value = ''
+}
+
+//display characters
+function display(name, image){
+    let characterName = document.querySelector('.characterInfo').firstElementChild.firstElementChild
+    let characterImage = characterName.nextElementSibling
+    characterName.textContent = name
+    characterImage.src = image
+    characterImage.nextElementSibling.firstElementChild.innerText = 0
+
+    // grab form to update votes based on character
+    let form = document.querySelector('.characterInfo').firstElementChild.lastElementChild.previousElementSibling
+    form.addEventListener('submit', submitVotes)
+}
+
+//reset votes
+function resetVotes(){
+    document.querySelector('.characterInfo').firstElementChild.querySelector('h4').firstElementChild.innerText = 0
+}
